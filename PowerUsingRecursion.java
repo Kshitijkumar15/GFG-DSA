bca9a71e1e448f8cbdf117df5b104763a410fd1a
Kshitij kumar singh <66892634+Kshitijkumar15@users.noreply.github.com>
2024-05-12 10:44:38 +0530

    Recursion

---
+++ PowerUsingRecursion.java
@@ -0,0 +1,13 @@
+class Solution
+{
+     int RecursivePower(int n, int p) {
+      
+        if (p == 0) {
+            return 1;
+        }
+    
+        else {
+            return n * RecursivePower(n, p - 1);
+        }
+}
+}
