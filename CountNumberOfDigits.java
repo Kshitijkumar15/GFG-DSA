059f598a1c6d00e82efe54391d85e954be1e7743
Kshitij kumar singh <66892634+Kshitijkumar15@users.noreply.github.com>
2024-05-14 07:22:54 +0530

    Create CountNumberOfDigits.java

---
+++ CountNumberOfDigits.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    
+    public static int countDigits(int n)
+    {
+      if (n < 10)
+        {
+            return 1;
+        }
+      
+      return 1 + countDigits(n / 10);
+    }
+}
